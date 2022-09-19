3f0a5cb3ba3c9901ccc48b66f46a88b6d06711d4
Ioannis Tsimpiris <ioannis.tsimpiris@kalibrate.com>
2022-09-19 22:51:13 +0100

    make soup

--- EIA_Data_Scraper.py
+++ EIA_Data_Scraper.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 def main():
     url = 'https://www.eia.gov/dnav/pet/pet_cons_prim_dcu_nus_m.htm'
     page = requests.get(url)
+    
+    soup = BeautifulSoup(page.text, 'lxml')
+    prettyHTML = soup.prettify()
+    print(prettyHTML)
 
 
 if __name__ == "__main__":
